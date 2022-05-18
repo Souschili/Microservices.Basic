018ac480f77c6adae4d934f75e50f4a4c1142f77
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-05-18 02:41:11 +0200

    Add project files.

---
+++ Microservice1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice1.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("index")]
+        public string Index()
+        {
+            return "String from service 1";
+        }
+    }
+}
---
+++ Microservice2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice2.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("index")]
+        public string Index()
+        {
+            return "String from service 2";
+        }
+    }
+}
